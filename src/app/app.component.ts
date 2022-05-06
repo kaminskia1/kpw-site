62e9d8ec3143c1b5b32fb5cb66a87c0af7cc7a06
kaminskia1 <8168918+kaminskia1@users.noreply.github.com>
2022-05-05 22:37:07 -0400

    Major cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,7 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  host: {
-    '(window:resize)': 'onResize()'
-  },
+  host: {},
   animations: [
     trigger('preload', [
       state('false', style({opacity: 1})),
@@ -18,14 +16,14 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       transition('false => true', animate("1000ms 500ms", style({opacity: 0})))
     ]),
 
-    trigger('imageLoaded', [
+    trigger('image', [
       state('false', style({opacity: 0})),
       state('true', style({opacity: 1})),
       transition('true => false', animate(".25s", style({opacity: 0}))),
       transition('false => true', animate(".25s .5s", style({opacity: 1})))
     ]),
 
-    trigger('locationLoaded', [
+    trigger('location', [
       state('false', style({width: "0px"})),
       state('true', style({})),
       transition('true => false', animate(".15s")),
@@ -40,7 +38,6 @@ export class AppComponent implements OnInit {
   backgroundObj = new Background();
   pageLoaded: boolean = false;
   imageLoaded: boolean = false;
-  locationLoaded: boolean = false;
   links = [
     {
       url: "https://kaminski.pw/resume",
@@ -66,18 +63,16 @@ export class AppComponent implements OnInit {
 
 
   @ViewChild('background') private backgroundElement: ElementRef | undefined;
-  @ViewChild('preload') private preloadElement: ElementRef | undefined;
 
   constructor(private backgroundService: BackgroundService) {
   }
 
   ngOnInit() {
-    this.setBackground().then(r => r);
+    this.setBackground();
   }
 
-  async setBackground() {
+  setBackground() {
     this.imageLoaded = false;
-    this.locationLoaded = false;
     this.backgroundService.getBackgroundUrl().subscribe(
       (response) => {
         this.backgroundService.getBackgroundImage(response.url).subscribe(
@@ -102,67 +97,9 @@ export class AppComponent implements OnInit {
       });
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-
-    let columnWidth = window.innerWidth / 2
-    let columnHeight = window.innerHeight;
-
-    // @ts-ignore
-    let eleWidth = this.backgroundElement.nativeElement.clientWidth;
-    // @ts-ignore
-    let eleHeight = this.backgroundElement.nativeElement.clientHeight;
-
-    if (eleWidth <= columnWidth || eleHeight > columnHeight + 1) {
-      this.backgroundObj.offsetWidth = "";
-      this.backgroundObj.offsetHeight = "-" + (eleHeight - columnHeight) / 2 + "px";
-      this.backgroundObj.width = columnWidth + "px";
-      this.backgroundObj.height = "";
-    } else {
-      this.backgroundObj.offsetWidth = "-" + (eleWidth - columnWidth) / 2 + "px";
-      this.backgroundObj.offsetHeight = "";
-      this.backgroundObj.width = "";
-      this.backgroundObj.height = "100%";
-    }
-  }
-
-  // @TODO: Incorporate this into onResize, as it's virtually identical
   onLoad() {
-
-    // @ts-ignore
-    let eleWidth = this.backgroundElement.nativeElement.naturalWidth;
-    // @ts-ignore
-    let eleHeight = this.backgroundElement.nativeElement.naturalHeight;
-
-    let windowWidth = window.innerWidth / 2
-    let windowHeight = window.innerHeight
-
-    // Compare image ratio to client's view box ratio
-    // Height / ClientHeight to ImgWidth / ClientWidth to determine which 100% will make image cover screen
-    if (eleHeight / windowHeight > eleWidth / windowWidth) {
-
-      // width == window.innerWidth / 2
-      let ratio = eleWidth / (window.innerWidth / 2)
-
-      this.backgroundObj.width = windowWidth + "px";
-      this.backgroundObj.height = eleHeight / ratio + "px";
-      this.backgroundObj.offsetWidth = "";
-      this.backgroundObj.offsetHeight = "-" + ((eleHeight / ratio) - windowHeight) / 2 + "px";
-
-    } else {
-      // height = window.innerHeight
-      let ratio = eleHeight / windowHeight;
-
-      this.backgroundObj.width = eleWidth / ratio + "px";
-      this.backgroundObj.height = windowHeight + "px";
-      this.backgroundObj.offsetWidth = "-" + ((eleWidth / ratio) - windowWidth) / 2 + "px";
-      this.backgroundObj.offsetHeight = "";
-
-    }
-
     this.pageLoaded = true;
     this.imageLoaded = true;
-    this.locationLoaded = true;
   }
 
 }
